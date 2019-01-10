12abee2c4a0df46cb90c9182c9c879ba3079b6b3
创客工场 <chuangkegongchang@xupea.local>
2019-01-10 10:29:56 +0800

    added ws

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var express = require("express");
+var expressWs = require("express-ws");
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+var expressWs = expressWs(express());
+var app = expressWs.app;
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+app.ws("/a", function(ws, req) {});
+var aWss = expressWs.getWss("/a");
+
+app.ws("/b", function(ws, req) {});
+
+app.ws("/", function(ws, req) {
+  ws.on("message", function(msg) {
+    console.log(msg);
   });
+  console.log("socket", req.testing);
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+// setInterval(function() {
+//   aWss.clients.forEach(function(client) {
+//     client.send("hello");
+//   });
+// }, 5000);
+
+app.listen(3000);
